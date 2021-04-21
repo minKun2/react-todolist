d694d384ef4285e736b9d56b8439ec24f4eca8ae
minjae <rlagurtns12@gmail.com>
2021-04-21 11:20:32 +0900

    해드 부분과 리스트 부분 단 나누기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 1fddddb7ec7a88a713deedefbb17caeddfb3774c
->>>>>>> 9c9d953ec78b741f199dfab4c00417c8decfc51e
 import React from "react";
 import { createGlobalStyle } from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background:  #e9ecef;
+    background: #e9ecef;
   }
 `;
 
@@ -19,7 +14,12 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <TodoTemplate>안녕하세요. todolist입니다.</TodoTemplate>
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+      </TodoTemplate>
     </>
   );
 }
+
+export default App;
