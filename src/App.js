080f1d81c663856a03058cd955861b7c075f946d
minjae <rlagurtns12@gmail.com>
2021-04-26 19:08:30 +0900

    API 기능 써보기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React from "react";
-import { createGlobalStyle } from "styled-components";
-import TodoTemplate from "./components/TodoTemplate";
-import TodoHead from "./components/TodoHead";
-import TodoList from "./components/TodoList";
-import TodoCreate from "./components/TodoCreate";
-import { TodoProvider } from "./TodoContext";
+// import React from "react";
+// import { createGlobalStyle } from "styled-components";
+// import TodoTemplate from "./components/TodoTemplate";
+// import TodoHead from "./components/TodoHead";
+// import TodoList from "./components/TodoList";
+// import TodoCreate from "./components/TodoCreate";
+// import { TodoProvider } from "./TodoContext";
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: #e9ecef;
-  }
-`;
+// const GlobalStyle = createGlobalStyle`
+//   body {
+//     background: #e9ecef;
+//   }
+// `;
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <GlobalStyle />
+//       <TodoTemplate>
+//         <TodoHead />
+//         <TodoList />
+//         <TodoCreate />
+//       </TodoTemplate>
+//     </TodoProvider>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Users from "./Users";
 
 function App() {
-  return (
-    <TodoProvider>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
-    </TodoProvider>
-  );
+  return <Users />;
 }
 
 export default App;
