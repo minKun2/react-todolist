7b5df6f65b63e7c3bdcaeeb052050f5c3631f459
minjae <rlagurtns12@gmail.com>
2021-05-18 16:39:48 +0900

    no message

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { MdFiberPin } from "react-icons/md";
 import styled from "styled-components";
 import TodoItem from "./TodoItem";
-
+import {useTodoState} from '../TodoContext';
+ 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
